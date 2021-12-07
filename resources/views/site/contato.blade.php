93fbd0da74365ee706f24892f16c5a250081b8b1
Tony Frezza <frezzatony@gmail.com>
2021-12-07 15:41:56 -0300

    primeiras rotas

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,7 @@
+<h1>
+    Página de contato
+</h1>
+<form method="post">
+    <input name="nome" type="text" />
+
+</form>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,111 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', function () {
+    return view('site.contato');
+});
+
+
+Route::get('/empresa', function () {
+    return 'Sobre a empresa';
+});
+
+Route::any('/any', function () {
+    return 'Rota Any';
+});
+
+Route::match(['get','post'],'/match', function () {
+    return 'Rota Match';
+});
+
+Route::get('/categorias/{categoria}', function ($categoria) {
+    return 'Caterogia: '.$categoria ;
+});
+
+Route::get('/produtos/{idProduct?}', function ($idProduct=NULL) {
+    return 'Produto(s): '.$idProduct ;
+});
+
+
+Route::redirect('/redirect01','/redirect02');
+//Route::get('/redirect01', function () {
+//    return redirect('/redirect02');
+//});
+
+Route::get('/redirect02', function () {
+    return 'Redirect 02';
+});
+
+Route::view('/view','welcome',['id'=>'valor do parametro id']);
+
+Route::get('/nome-url', function () {
+    
+    return 'Nome uurl';
+    
+})->name('url.name');
+
+
+Route::get('/redirect03', function(){
+    
+    return redirect()->route('url.name');
+    
+});
+
+Route::get('/login', function () {
+    return 'Login';
+})->name('login');
+
+/*
+Route::middleware([])->group(function(){
+    
+    Route::prefix('admin')->group(function(){
+        
+        Route::namespace('Admin')->group(function(){
+            
+            Route::name('admin.')->group(function(){
+                
+                Route::get('/',function(){
+                    
+                    return redirect()->route('admin.dashboard');
+                });  
+                
+                Route::get('/dashboard', 'MainController@main')->name('dashboard'); 
+        
+                Route::get('/financeiro', 'FinanceiroController.php@main')->name('financeiro');
+                
+                Route::get('/produtos', 'ProdutosController@main')->name('produtos');
+                
+                  
+            });
+            
+             
+        });
+    });   
+    
+});
+*/
+
+Route::group(
+    [
+        'middleware'    =>  [], //auth, ...
+        'prefix'        =>  'admin',
+        'namespace'     =>  'Admin',
+        'name'          =>  'admin.'
+    ],
+    function(){
+        
+        Route::get('/',function(){
+                    
+            return redirect()->route('admin.dashboard');
+        });  
+        
+        Route::get('/dashboard', 'MainController@main')->name('dashboard'); 
+
+        Route::get('/financeiro', 'FinanceiroController@main')->name('financeiro');
+        
+        Route::get('/produtos', 'ProdutosController@main')->name('produtos');        
+        
+    }
+);
+
